68ab3ec27a78bfd1fa5e8137130b5a1fdcdf89d5
yrq110 <yrq1025@gmail.com>
2016-11-13 12:02:18 +0800

    add window-event

--- 1.window/assets/js/index.js
+++ 1.window/assets/js/index.js
@@ -3,6 +3,7 @@ const path = require('path')
 const fs = require('fs')
 const highlight = require('highlight.js')
 
+// 1.create-window
 var cw_btn = document.querySelector('.creat-window-button');
 cw_btn.addEventListener('click', function () {
 	const modalPath = path.join(`file://${__dirname}/index.html`)
@@ -12,6 +13,7 @@ cw_btn.addEventListener('click', function () {
 	win.show()
 });
 
+// 2.indow-state
 var ws_btn = document.querySelector('.window-state-button');
 ws_btn.addEventListener('click', function () {
 	const modalPath = path.join(`file://${__dirname}/index.html`)
@@ -28,7 +30,33 @@ ws_btn.addEventListener('click', function () {
   }
 });
 
+// 3.window-event
+var we_btn = document.querySelector('.window-event-button');
+var focusModalBtn = document.querySelector('.focus-on-button');
+we_btn.addEventListener('click', function () {
+	const modalPath = path.join(`file://${__dirname}/index.html`)
+  let win = new BrowserWindow({ width: 600, height: 400 })
+  win.on('focus', hideFocusBtn)
+  win.on('blur', showFocusBtn)
+  win.on('close', function () {
+    hideFocusBtn()
+    win = null
+  })
+  win.loadURL(modalPath)
+  win.show()
+  function showFocusBtn (btn) {
+    if (!win) return
+    focusModalBtn.classList.add('smooth-appear')
+    focusModalBtn.classList.remove('disappear')
+    focusModalBtn.addEventListener('click', function () { win.focus() })
+  }
+  function hideFocusBtn () {
+    focusModalBtn.classList.add('disappear')
+    focusModalBtn.classList.remove('smooth-appear')
+  }
+});
 
+// highlight code block
 const codeBlocksWithPaths = document.querySelectorAll('code[data-path]')
 
 Array.prototype.forEach.call(codeBlocksWithPaths, function (code) {
